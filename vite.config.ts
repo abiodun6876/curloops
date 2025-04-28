44c9897e39276a23bf0394aa69a618e255f93665
abiodun6876 <abiodunmosesakinpelu@gmail.com>
2025-04-28 05:52:47 +0100

    Describe the changes you made here

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   base: './', // <<<<<<<<<< VERY IMPORTANT for Netlify (relative paths)
   optimizeDeps: {
     exclude: ['lucide-react'],
